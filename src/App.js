f679a85dabb5da1cb197ffa849e340a1a60d38a6
Ratthamanoon <s6703052411431@email.kmutnb.ac.th>
2025-12-18 11:58:19 +0700

    Update App.js with App.css

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <table border="1">
-      <th>Product</th>
-      <th>Price</th>
-
-      <tr>
-        <td>React</td>
-        <td>320</td>
-      </tr>
-
-      <tr>
-        <td>React Native</td>
-        <td>340</td>
-      </tr>
-    </table>
-  );
+  const divStyle = {
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSiza:'larger',
+    padding:'3px',
+  }
+  return <div style={divStyle}>Hello Reacth</div>
 }
 
 export default App;
