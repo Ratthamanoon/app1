f99efe4411e8543334c1bb20f51c7332b9a80de4
Ratthamanoon <s6703052411431@email.kmutnb.ac.th>
2025-12-25 12:20:26 +0700

    Class-Components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import './style.css'
 import { Content, Footer, Header } from "./func-component";
+import Calendar from "./Class-Components";
 
 function App() {
  return <>
  <Header/>
+ <p><center><Calendar/></center></p>
  <Content/>
  <Footer/>
  </>
