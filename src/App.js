9e640a348c3890a839bde143aa87d761b04d70c8
Ratthamanoon <s6703052411431@email.kmutnb.ac.th>
2026-01-08 10:52:45 +0700

    ButtonAlert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import './style.css'
-import { Content, Footer, Header } from "./func-component";
-import Calendar from "./Class-Components";
+import Button from "./Class-Components";
 
 function App() {
- return <>
- <Header/>
- <p><center><Calendar/></center></p>
- <Content/>
- <Footer/>
- </>
+    return <>
+    <Button/>
+</>
 }
+    
 
 export default App;
